d9bdc7d204454ca9fda54c825782b7965b6947eb
Leonardo Dias Miranda <ldiasmir@gmail.com>
2021-04-25 22:01:19 -0300

    Fixing variable name

--- src/tvshows/views.py
+++ src/tvshows/views.py
@@ -4,11 +4,13 @@ from poc import utils
 
 # Create your views here.
 def tvshows_list_view(request):
-    resultado = utils.api_page_append(5)
+	#list of the top 100 most popular tv shows
+    context = utils.api_page_append(5)
 
-    return render(request, "tvshows_list.html", resultado)
+    return render(request, "tvshows_list.html", context)
 
 def tvshows_detail_view(request, tv_id):
+	#tv show details
 	rating_form = utils.rating_form()
 	if request.method == 'POST':
 		rating_form = utils.rating_form(request.POST)
